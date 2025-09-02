020d1b90e7ddf0537597d1413aeed2ba32118297
Soumikdey81 <aditdey30@gmail.com>
2025-09-02 19:26:22 +0530

    pysparks

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,4 +1,4 @@
-import { FaFacebookF, FaLinkedin } from "react-icons/fa";
+import { FaFacebookF, FaInstragram, FaLinkedin } from "react-icons/fa";
 import styles from "./Socials.module.css";
 
 export default function Socials() {
@@ -13,7 +13,15 @@ export default function Socials() {
         <FaFacebookF />
       </a>
       <a
-        href="https://facebook.com"
+        href="https://www.instagram.com/pysparks_lab.llp/"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Instragram"
+      >
+        <FaInstragram />
+      </a>
+      <a
+        href="https://www.linkedin.com/posts/pysparks-lab_businesssolutions-innovation-ai-activity-7368331457406136322-Z4Fn/"
         target="_blank"
         rel="noreferrer"
         aria-label="Linkedin"
